23e56b616b4853c7e39ac9e16edaa71ba5909020
psnluiz <psnluiz@hotmail.com>
2023-02-04 00:17:51 -0300

    Initial Commit

---
+++ Onde Importa/Onde_ImportaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Onde_ImportaApp.swift
+//  Onde Importa
+//
+//  Created by Luiz Fernando Andrade on 04/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Onde_ImportaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Onde ImportaUITests/Onde_ImportaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Onde_ImportaUITestsLaunchTests.swift
+//  Onde ImportaUITests
+//
+//  Created by Luiz Fernando Andrade on 04/02/23.
+//
+
+import XCTest
+
+final class Onde_ImportaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
