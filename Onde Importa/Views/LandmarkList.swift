1d4d1bfdab9d4873e8470daa8bd1d548c6712136
psnluiz <psnluiz@hotmail.com>
2023-02-08 01:49:49 -0300

    feat: list of landmarks

---
+++ Onde Importa/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Onde Importa
+//
+//  Created by Luiz Fernando Andrade on 08/02/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
